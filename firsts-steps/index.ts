1d373e4ca659854f7595670612c528d7f22502de
Lachicagradiadora <200379@unsaac.edu.pe>
2023-05-31 08:27:49 -0500

    variable let

---
+++ firsts-steps/index.ts
@@ -0,0 +1,37 @@
+// HELLO WORLD
+let message: string = 'HELLO WORLD!';
+console.log(message); //HELLO WORLD!
+
+// TYPE OF VARIABLES
+let book:string = 'Pragmatic Programmer';
+let decimal:number = 34.43;
+let authorizing:boolean = true;
+console.log(book, decimal, authorizing)
+
+//  create LISTS
+let numbers: number[] = [43, 65, 12, 234, 87, 2, 23, 87654, 12345]
+console.log(numbers)
+
+// create type of DATA MIXT
+type mixt = string | number | boolean | string[];
+let mixtData: mixt[] = ['text', 24, true, ['list from other list']];
+console.log(mixtData)
+
+// maps OBJECTS or DICTIONARIES: they allow us to access a property using a key
+let gamers:{[key: number]: string} = {
+  10: 'Messi',
+  7: 'Christiano Ronaldo'
+}
+console.log(gamers)
+let countries: Record<string, string> = {
+  EC: 'Ecuador',
+  PE: 'Peru',
+  AR: 'Argentina',
+  MX: 'Mexico'
+}
+console.log(countries)
+let users: Record <string, string[]> ={
+  'Juan': ['juanito_2@gmail.com'],
+  'Ricardo': ['ricardo@gmail.com', 'recardito@hotmail.com']
+}
+console.log(users)
--- src/mutiplicator.ts
+++
@@ -1,5 +0,0 @@
-const multiplication = (a:number, b: number, printText: string) => {
-  console.log(printText, a * b)
-}
-
-multiplication(2, 4, 'Multiplied a 2 and 4, the result is: ')
