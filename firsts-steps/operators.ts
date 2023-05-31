fd25321b34b839ea2972d1a4e5ffac5220f72e00
Lachicagradiadora <200379@unsaac.edu.pe>
2023-05-31 09:19:35 -0500

    operators

---
+++ firsts-steps/operators.ts
@@ -0,0 +1,20 @@
+// OPERATORS  -> symbols that allow you to perform operations
+// arithmetic operators ... 
+console.log(1+1);
+console.log(1-5);
+console.log(9*4);
+console.log(16/4);
+// ... or comparative operators ...
+console.log(4 == 4);
+console.log(4 < 5);
+console.log(4 >= 5);
+// ... or logical operators 
+console.log(true && true)
+console.log(true && false)
+console.log(false && true)
+console.log(false && false)
+
+console.log(true || true)
+console.log(true || false)
+console.log(false || true)
+console.log(false || false)
