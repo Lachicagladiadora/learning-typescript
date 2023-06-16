8e01a5464d43b9c90e75904d45786f63c3623ec1
Lachicagladiadora <200379@unsaac.edu.pe>
2023-06-16 18:47:04 -0500

    complete, and missing styles

---
+++ todo-list/src/components/VisibilityControl.tsx
@@ -0,0 +1,30 @@
+import { ChangeEvent } from "react"
+import { Button } from "./Button"
+
+export const VisibilityControl = ({setShowCompleted,cleanTasks,isChecked}:any) => {
+  const closeShow = () => {
+    // alert('clearing')
+    if(window.confirm('Are you sure you want to delete it?')) cleanTasks()
+  }
+
+  return(
+    <div
+    style={{
+      color:'#B31D65',
+      textAlign:'center',
+      fontWeight:'bold',
+
+    }}
+    >
+      <label
+      >
+      <input 
+        type="checkbox" 
+        checked={isChecked}
+        onChange={(_e: ChangeEvent) => setShowCompleted(_e.target.checked)} 
+      />{ ' ' }Show Tasks Done{ ' ' }
+      </label>
+      <Button children='Clear' onClick={closeShow}/>
+    </div >
+  )
+}
